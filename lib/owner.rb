e53e9e5c0f35184690b02a128bc4f41d756c490f
kevin lang <langkevina@gmail.com>
2019-06-11 01:06:53 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -61,10 +61,10 @@ class Owner
   end
 
   def sell_pets
-    pets.each do |type, pets|
-      pets.map {|pet| pet.mood = "nervous"}
+    @pets.each do |type, pets|
+      @pets.map {|pet| pet.mood = "nervous"}
     end
-    pets.clear
+    @pets.clear
     # @pets[:fishes].clear
     # @pets[:dogs].clear
     # @pets[:cats].clear
